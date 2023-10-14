e2786f195cd1c86d308a016f177559259fa57c23
9walnut <kguho9202@gmail.com>
2023-10-14 17:52:00 +0900

    Study: promise

--- node-test/hard/parctice.js
+++ node-test/hard/parctice.js
@@ -48,12 +48,13 @@ async function exec() {
 async function changeColor(color) {
   return new Promise(function (resolve, reject) {
     setTimeout(function () {
-      document.body.style.backgroundColor = color
+      document.body.style.backgroundColor = color;
       resolve();
     }, 1000);
+  });
+}
 
-
-async function exec(){
+async function exec2() {
   await changeColor("red");
   await changeColor("orane");
   await changeColor("yellow");
@@ -61,4 +62,4 @@ async function exec(){
   await changeColor("blue");
 }
 
-exec()
+exec2();
